c549aac8cb44d1b75e7bf3c95ce09fffe011f65c
rishiraila <rishiraila007@gmail.com>
2025-10-17 15:07:55 +0530

    changes

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -8,7 +8,7 @@ export default function Contact() {
         <h1>Contact Us</h1>
         <p>If you have any questions about this Privacy Policy, please contact us at: Coinage Inc.</p>
         <p><strong>Address:</strong></p>
-        <p>Office no 106, Excellaa Plazzo, Near Mumbai Hwy, Ambegaon Budruk, Pune, Maharashtra 411046</p>
+        <p>Office no 106, Excellaa Plazzo, Near Mumbai Hwy, Ambegaon Bk, Pune, Maharashtra 411046</p>
         <p><strong>Phone:</strong></p>
         <p>+91 915 828 7488</p>
         <p><strong>Email:</strong></p>
